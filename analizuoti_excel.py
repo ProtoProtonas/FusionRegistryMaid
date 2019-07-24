99239c6b096ef03a8f6d60472799b07f4aa2417a
Augustas <augis.seras@gmail.com>
2019-07-24 16:44:20 +0300

    Update analizuoti_excel.py

--- analizuoti_excel.py
+++ analizuoti_excel.py
@@ -4,62 +4,102 @@ import time
 
 from bs4 import BeautifulSoup as bs
 from colorama import init, Back, Fore
-from freg_funkcijos import normalize_text, openxml, register_namespaces, remove_version_et, remove_version_str
+from freg_funkcijos import normalize_text, openxml, register_namespaces, remove_version_et, remove_version_str, sortCode
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Alignment
 from xml.etree.ElementTree import Element, ElementTree
-
-def open_xlsx(filename):
-    wb = load_workbook(filename)
-    return wb.active
 
 def parse_str_to_element(child):
     elem = et.fromstring(child)
     return elem
 
+def add_child_to_codelist(codelists, parent_id, child):
+    for codelist in codelists:
+        for code in codelist:
+            try:
+                if parent_id in code.get('urn'):
+                    codelist.remove(code)
+                    codelist.append(child)
+                    return
+            except:
+                pass
 
-def main(filename):
+
+def main(file):
     init()
-    ws = open_xlsx('%s.xlsx' % filename)
-    
-    rt = openxml('%s.xml' % filename)
+    rt = openxml('new_' + file)
     _, codelists = list(rt)
 
+    filenames = os.listdir('excel_raktai')
 
-    rows = list(ws.iter_rows())
-    for i, row in enumerate(rows):
-        # kas antroje eilutėje yra raktai ir konfliktuojančios jų reikšmės
-        if i % 2 == 0:
-            parent = row[0].value
-        else:
-            row_value = ''
+    for filename in filenames:
+        wb = load_workbook(os.path.join('excel_raktai', filename))
+        ws_xml = wb['XML']
+        ws_pav = wb['Pavadinimai']
 
-            for elem in row:
-                if elem.value != None:
-                    row_value = elem.value # row_value - vienoje eilutėje esančio snippeto reikšmė
+        rows = list(ws_pav.iter_rows())
+        keys = {}
+        for i, row in enumerate(rows):
+            key_lt = ''
+            key_en = ''
 
-            child = row_value
-            child = parse_str_to_element(child) # string paverčiamas į etree.elemeent objektą
-            
-            # ieškoma codelisto su tokiu pačiu id, kaip parent
-            for codelist in codelists:
-                for code in codelist:
-                    if 'urn' in code.attrib:
-                        code_id = code.attrib['urn']
-                        code_id = code_id.split(':')[-1]
-                        code_id = remove_version_str(code_id)
-                        if code_id == parent:
-                            # pašalinamas kažkoks kodas (galimai ir teisingas) ir jo vietoj pridedamas tikrai teisingas
-                            codelist.remove(code)
-                            codelist.append(remove_version_et(child))
-                            break
+            if i % 3 == 0: # parent elemento ID
+                parent_id = row[0].value
+                parent_id = filename.split('.')[0] + '(1.0).' + parent_id
+                keys[parent_id] = {}
 
+            elif i % 3 == 1: # lietuviška rakto reikšmė
+                for cell in row:
+                    if cell.value != None and cell.column % 2 == 0:
+                        keys[parent_id]['lt'] = cell.value
+            else: # angliška rakto reikšmė
+                for cell in row:
+                    if cell.value != None and cell.column % 2 == 0:
+                        keys[parent_id]['en'] = cell.value
+
+        rows = list(ws_xml.iter_rows())
+        for i, row in enumerate(rows):
+            if i % 2 == 0:
+                parent_id = row[0].value
+
+            else:
+                child = str(row[0].value)
+
+                if 'lt' in keys[parent_id] and 'en' in keys[parent_id]:
+                    child = child.replace('###lt###', keys[parent_id]['lt'])
+                    child = child.replace('###en###', keys[parent_id]['en'])
+
+                elif 'lt' in keys[parent_id] and not 'en' in keys[parent_id]:
+                    child = child.replace('###lt###', keys[parent_id]['lt'])
+
+                elif 'en' in keys[parent_id] and not 'lt' in keys[parent_id]:
+                    child = child.replace('###en###', keys[parent_id]['en'])
+
+                else:
+                    print('Galima klaida %s' % parent_id)
+                    
+                # čia pakeisti xml failą
+                child = parse_str_to_element(child)
+                add_child_to_codelist(codelists, parent_id, child)
+
+
+    # išrikiuoti abėcėlės tvarka
+
+    for codelist in codelists:
+        pass
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+    # ir jį išsaugoti
     final_string = et.tostring(rt)
-    with open('%s.xml' % filename, 'wb') as f:
+    with open('new_' + file, 'wb') as f:
         f.write(final_string)
 
     print(Back.GREEN + 'Failas išsaugotas sėkmingai' + Back.BLACK)
+    _ = input()
 
 
 
-main('new_small_codelist')
+main('small_codelist.xml')
