1f9bbdcb1de29e241f81083ca7a4e28a725f1d69
Augustas <augis.seras@gmail.com>
2019-07-25 16:27:54 +0300

    Update analizuoti_excel.py

--- analizuoti_excel.py
+++ analizuoti_excel.py
@@ -11,6 +11,7 @@ from xml.etree.ElementTree import Element, ElementTree
 
 def parse_str_to_element(child):
     elem = et.fromstring(child)
+    elem = remove_version_et(elem)
     return elem
 
 def add_child_to_codelist(codelists, parent_id, child):
@@ -49,11 +50,11 @@ def main(file):
                 keys[parent_id] = {}
 
             elif i % 3 == 1: # lietuviška rakto reikšmė
-                for cell in row:
+                for cell in reversed(row):
                     if cell.value != None and cell.column % 2 == 0:
                         keys[parent_id]['lt'] = cell.value
             else: # angliška rakto reikšmė
-                for cell in row:
+                for cell in reversed(row):
                     if cell.value != None and cell.column % 2 == 0:
                         keys[parent_id]['en'] = cell.value
 
@@ -69,14 +70,16 @@ def main(file):
                     child = child.replace('###lt###', keys[parent_id]['lt'])
                     child = child.replace('###en###', keys[parent_id]['en'])
 
-                elif 'lt' in keys[parent_id] and not 'en' in keys[parent_id]:
+                elif 'lt' in keys[parent_id] and not 'en' in keys[parent_id]: # jei egzistuoja tik lietuviška rakto reikšmės versija
                     child = child.replace('###lt###', keys[parent_id]['lt'])
+                    print('Galima klaida „%s“. Nėra angliško kodo aprašymo' % parent_id)
 
-                elif 'en' in keys[parent_id] and not 'lt' in keys[parent_id]:
+                elif 'en' in keys[parent_id] and not 'lt' in keys[parent_id]: # jei egzistuoja tik angliška rakto reikšmės versija
                     child = child.replace('###en###', keys[parent_id]['en'])
+                    print('Galima klaida „%s“. Nėra lietuviško kodo aprašymo' % parent_id)
 
                 else:
-                    print('Galima klaida %s' % parent_id)
+                    print('Klaida „%s“. Nėra reikšmės nei viena kalba' % parent_id)
                     
                 # čia pakeisti xml failą
                 child = parse_str_to_element(child)
@@ -86,11 +89,19 @@ def main(file):
     # išrikiuoti abėcėlės tvarka
 
     for codelist in codelists:
-        pass
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        codes = []
+        for code in codelist:
+            codes.append(code) # sukuria sąrašą su visais jau esamais kodais
 
+        codes.sort(key = sortCode) 
 
+        for code in codes:
+            codelist.append(code) # prie seno kodų sąrašo prideda naują, surikiuotą sąrašą
 
+        total_codes = len(codes)
+        
+        for _ in range(total_codes):
+            codelist.remove(codelist[0]) # lauk išmetami nesurikiuoti kodai (kai nulinis nuimamas tuomet viskas pasislenka per vieną)
 
     # ir jį išsaugoti
     final_string = et.tostring(rt)
