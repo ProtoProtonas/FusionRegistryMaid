12bd04af82ab9e79035b2c1b3ac0730a4446e297
Augustas <augis.seras@gmail.com>
2019-07-30 10:50:37 +0300

    finalest final

--- analizuoti_excel.py
+++ analizuoti_excel.py
@@ -4,15 +4,27 @@ import time
 
 from bs4 import BeautifulSoup as bs
 from colorama import init, Back, Fore
-from freg_funkcijos import normalize_text, openxml, register_namespaces, remove_version_et, remove_version_str, sortCode
+from freg_funkcijos import normalize_text, openxml, print_xml, register_namespaces, remove_version_et, remove_version_str, sortCode
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Alignment
 from xml.etree.ElementTree import Element, ElementTree
 
 def parse_str_to_element(child):
-    elem = et.fromstring(child)
-    elem = remove_version_et(elem)
-    return elem
+    if '######' not in child:
+        elem = et.fromstring(child)
+        elem = remove_version_et(elem)
+        return [elem]
+    else:
+        children = child.split('######')
+        to_return = []
+        for ch in children:
+            elem = et.fromstring(ch)
+            elem = remove_version_et(elem)
+            to_return.append(elem)
+
+        return to_return
+        
+
 
 def add_child_to_codelist(codelists, parent_id, child):
     for codelist in codelists:
@@ -21,10 +33,14 @@ def add_child_to_codelist(codelists, parent_id, child):
                 if parent_id in code.get('urn'):
                     codelist.remove(code)
                     codelist.append(child)
-                    return
+                    break
             except:
-                pass
-
+                for key in code.attrib:
+                    if code.attrib[key] in (child.attrib).values():
+                        if parent_id.split('.')[0] in codelist.attrib['id']:
+                            codelist.remove(code)
+                            codelist.append(child)
+                            break
 
 def main(file):
     init()
@@ -46,7 +62,10 @@ def main(file):
 
             if i % 3 == 0: # parent elemento ID
                 parent_id = row[0].value
-                parent_id = filename.split('.')[0] + '(1.0).' + parent_id
+                if 'pavadinimas' not in parent_id:
+                    parent_id = filename.split('.')[0] + '(1.0).' + parent_id
+                else:
+                    parent_id = filename.split('.')[0] + '.' + parent_id
                 keys[parent_id] = {}
 
             elif i % 3 == 1: # lietuviška rakto reikšmė
@@ -62,29 +81,28 @@ def main(file):
         for i, row in enumerate(rows):
             if i % 2 == 0:
                 parent_id = row[0].value
-
             else:
                 child = str(row[0].value)
 
                 if 'lt' in keys[parent_id] and 'en' in keys[parent_id]:
-                    child = child.replace('###lt###', keys[parent_id]['lt'])
-                    child = child.replace('###en###', keys[parent_id]['en'])
+                    child = child.replace('###lt###', normalize_text(keys[parent_id]['lt']))
+                    child = child.replace('###en###', normalize_text(keys[parent_id]['en']))
 
                 elif 'lt' in keys[parent_id] and not 'en' in keys[parent_id]: # jei egzistuoja tik lietuviška rakto reikšmės versija
-                    child = child.replace('###lt###', keys[parent_id]['lt'])
+                    child = child.replace('###lt###', normalize_text(keys[parent_id]['lt']))
                     print('Galima klaida „%s“. Nėra angliško kodo aprašymo' % parent_id)
 
                 elif 'en' in keys[parent_id] and not 'lt' in keys[parent_id]: # jei egzistuoja tik angliška rakto reikšmės versija
-                    child = child.replace('###en###', keys[parent_id]['en'])
+                    child = child.replace('###en###', normalize_text(keys[parent_id]['en']))
                     print('Galima klaida „%s“. Nėra lietuviško kodo aprašymo' % parent_id)
 
                 else:
                     print('Klaida „%s“. Nėra reikšmės nei viena kalba' % parent_id)
                     
                 # čia pakeisti xml failą
-                child = parse_str_to_element(child)
-                add_child_to_codelist(codelists, parent_id, child)
-
+                children = parse_str_to_element(child)
+                for child in children:
+                    add_child_to_codelist(codelists, parent_id, child)
 
     # išrikiuoti abėcėlės tvarka
 
@@ -105,12 +123,10 @@ def main(file):
 
     # ir jį išsaugoti
     final_string = et.tostring(rt)
-    with open('new_' + file, 'wb') as f:
+    with open('new_final_' + file, 'wb') as f:
         f.write(final_string)
 
     print(Back.GREEN + 'Failas išsaugotas sėkmingai' + Back.BLACK)
-    _ = input()
+    #_ = input()
 
-
-
-main('small_codelist.xml')
+main('codelist.xml')
